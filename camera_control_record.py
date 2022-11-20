315e3e91951cda15a614e27949f08d5bafbfa7b3
Ren Zhenyu <2391928709@qq.com>
2022-11-20 16:42:41 +0800

    update test

---
+++ camera_control_record.py
@@ -0,0 +1,59 @@
+import cv2
+import pyzed.sl as sl
+
+camera_settings = sl.VIDEO_SETTINGS.BRIGHTNESS
+str_camera_settings = "BRIGHTNESS"
+step_camera_settings = 1
+
+
+def main():
+    print("Running...")
+    init = sl.InitParameters()
+    init.camera_resolution = sl.RESOLUTION.HD1080
+    init.depth_mode = sl.DEPTH_MODE.NONE
+    cam = sl.Camera()
+    if not cam.is_opened():
+        print("Opening ZED Camera...")
+    status = cam.open(init)
+    if status != sl.ERROR_CODE.SUCCESS:
+        print(repr(status))
+        exit()
+    print_camera_information(cam)
+    a = input('Press enter to begin recording.')
+    runtime = sl.RuntimeParameters()
+    mat = sl.Mat()
+    record(cam, runtime, mat, filepath="./data/Walk/walk1.svo", nframe=120)
+    cam.close()
+
+def print_camera_information(cam):
+    print("Resolution: {0}, {1}.".format(round(cam.get_camera_information().camera_resolution.width, 2), cam.get_camera_information().camera_resolution.height))
+    print("Camera FPS: {0}.".format(cam.get_camera_information().camera_fps))
+    print("Firmware: {0}.".format(cam.get_camera_information().camera_firmware_version))
+    print("Serial number: {0}.\n".format(cam.get_camera_information().serial_number))
+
+def record(cam, runtime, mat, filepath, nframe):
+    vid = sl.ERROR_CODE.FAILURE
+    out = False
+    while vid != sl.ERROR_CODE.SUCCESS and not out:
+        record_param = sl.RecordingParameters(filepath)
+        vid = cam.enable_recording(record_param)
+        print(repr(vid))
+        if vid == sl.ERROR_CODE.SUCCESS:
+            print("Recording started...")
+            frames_recorded = 0
+            while True:  # for spacebar
+                err = cam.grab(runtime)
+                if err == sl.ERROR_CODE.SUCCESS:
+                    frames_recorded += 1
+                    print("Frame count: " + str(frames_recorded), end="\r") 
+                    cam.retrieve_image(mat,sl.VIEW.LEFT)
+                    cv2.imshow("ZED", mat.get_data())
+                    key = cv2.waitKey(5)
+                    if frames_recorded == nframe:
+                        break
+    cam.disable_recording()
+    print("Recording finished.")
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ commands.md
@@ -0,0 +1,29 @@
+### Record `.svo`
+
+``` 
+python3 camera_control_record.py
+```
+
+### Post Processing
+
+Play 3-d skeletons from `.svo`.
+
+```
+python3 body_tracking_real_time_svo.py ./test.svo
+```
+
+Export 3-d skeletons to `.mat` from local recorded `.svo`.
+
+```
+python3 svo_skeletons.py
+```
+
+Export `.svo` to `.avi` videos.
+
+```
+python3 svo_export.py ./test.svo ./videos/left_right.mp4 0
+python3 svo_export.py ./test.svo ./videos/left_rightdepth.mp4 1
+```
+
+
+
