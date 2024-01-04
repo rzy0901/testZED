757ae846c68c17622d4a1947a55dfce42388b721
Ren Zhenyu <2391928709@qq.com>
2024-01-04 20:50:06 +0800

    reformat codes and fix cuda crash bugs by introducing svo_skeleton_list_v2.py

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # testZED
 
+## Introduction
 + Using a ZED 2i to convert `.svo` file to 3d skeletons.
 
-+ [ZED SDK](https://www.stereolabs.com/developers/release/) should be installed and `HD1080_SN35745898_19-28-43.svo` is placed in `/home/rzy/Documents/ZED/`.
-  + ZED SDK 4.0.1 is used in this repository.
++ [ZED SDK](https://www.stereolabs.com/developers/release/) should be installed.
+  + ZED SDK 4.0.8 is used in this repository.
   + [Blog post](https://rzy0901.github.io/post/zed/) for my notes.
-
-+ Usage: See [commands.md](./commands.md)
 
 + Screenshots:
 
@@ -19,7 +18,7 @@
 	</tbody>
 </table>
 
-<table>
+<!-- <table>
 	<tbody>
 		<tr>
 			<td>walk1</td>
@@ -36,6 +35,57 @@
 			<td><img src="https://github.com/rzy0901/testZED/blob/main/README.assets/squat.gif" alt="image";" /></td>
 		</tr>
 	</tbody>
-</table>
+</table> -->
 
-+ Acknowledgement: Thanks to [Li-baster](https://github.com/QianrenLi) for acting as a model for the recording of the video.
+## Usage
+
+### Record `.svo`
+
+``` 
+python3 camera_control_record.py
+```
+
+### Post Processing
+
+Play 3-d skeletons from `.svo`:
+
+```
+python3 body_tracking_real_time_svo.py ./test.svo
+```
+
+Export 3-d skeletons to `.mat` from local recorded `.svo`:
+
+```
+python3 svo_skeletons.py -i <input_svo_path> -o <output_mat_path> -b <body_format>
+```
+
+```
+❯ python3 svo_skeletons.py -h
+
+usage: svo_skeletons.py [-h] [-i INPUT_SVO_PATH] [-o OUTPUT_MAT_PATH] [-b BODY_FORMAT]
+
+Convert svo to mat.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT_SVO_PATH, --input_svo_path INPUT_SVO_PATH
+                        Input svo path, default:./test.svo
+  -o OUTPUT_MAT_PATH, --output_mat_path OUTPUT_MAT_PATH
+                        Output: mat path, default:./data/temp.mat
+  -b BODY_FORMAT, --body_format BODY_FORMAT
+                        Capatured body format, available selections: BODY_18, BODY_34, BODY_38; default:BODY_18
+```
+
+Export `.svo` to `.mp4` videos:
+
+```
+python3 svo_export.py ./test.svo ./videos/left_right.mp4 0
+python3 svo_export.py ./test.svo ./videos/left_rightdepth.mp4 1
+```
+
+
+
+
+
+## Acknowledgement: 
+Thanks to [Li-baster](https://github.com/QianrenLi) for acting as a model for the recording of the video.
--- svo_skeletons_list.py
+++ svo_skeletons_list.py
@@ -1,91 +1,44 @@
 import os
 import pyzed.sl as sl
-from scipy.io import savemat
 from tqdm import tqdm, trange
+import shutil
+from .svo_skeletons import cam_init, cam2mat
 
-
-def copyDirectory_createMat(file_dir,output_dir):
+def copyDirectory_createMat(cam,file_dir,output_dir,body_format='BODY_18'):
     obj = os.scandir(file_dir)
     count = 1
-    for entry in tqdm(list(obj),desc=file_dir):
+    # for entry in tqdm(list(obj),desc=file_dir):
+    for entry in list(obj):
         if entry.is_dir():
             os.makedirs(os.path.join(output_dir,entry.name),exist_ok=True)
-            copyDirectory_createMat(entry.path,os.path.join(output_dir,entry.name))
+            copyDirectory_createMat(cam,entry.path,os.path.join(output_dir,entry.name))
         if entry.is_file() and entry.name.endswith('.svo'):
             # temp = entry.name.split(".")
             # output_path = os.path.join(output_dir,temp[-2]+".mat")
             # 重命名为'run1.mat'，'run2.mat',...
             temp = os.path.basename(file_dir)
-            output_path = os.path.join(output_dir,temp+"_"+str(count)+".mat")
+            output_mat_path = os.path.join(output_dir,temp+"_"+str(count)+".mat")
             count += 1
-            svo2mat(entry.path,output_path)
+            cam_init(cam,entry.path,body_format)
+            ERROR,_,_,_ = cam2mat(cam,output_mat_path)
+            if ERROR == 0:
+                print(f"Successfully convert {entry.path} to {output_mat_path}")
+            else:
+                print(f"ERROR:No data output in {output_mat_path}")
+            cam.disable_object_detection()
+            cam.disable_positional_tracking()
+            cam.close()
     obj.close()
 
-def svo2mat(svo_path,output_path):
-    zed = sl.Camera()
-    init_params = sl.InitParameters()
-    init_params.camera_resolution = sl.RESOLUTION.HD1080  # Use HD1080 video mode
-    init_params.coordinate_units = sl.UNIT.METER          # Set coordinate units
-    init_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE
-    init_params.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
-    # print("Using SVO file: {0}".format(svo_path))
-    init_params.svo_real_time_mode = True # Real Time Modes would drop some frames.
-    init_params.set_from_svo_file(svo_path)
-    err = zed.open(init_params)
-    if err != sl.ERROR_CODE.SUCCESS:
-        exit(1)
-    positional_tracking_parameters = sl.PositionalTrackingParameters()
-    positional_tracking_parameters.set_as_static = True
-    zed.enable_positional_tracking(positional_tracking_parameters)
-    body_param = sl.BodyTrackingParameters()
-    body_param.enable_tracking = True                # Track people across images flow
-    body_param.enable_body_fitting = True            # Smooth skeleton move
-    body_param.detection_model = sl.BODY_TRACKING_MODEL.HUMAN_BODY_ACCURATE 
-    body_param.body_format = sl.BODY_FORMAT.BODY_34  # Choose the BODY_FORMAT you wish to use
-    zed.enable_body_tracking(body_param)
-    body_runtime_param = sl.BodyTrackingRuntimeParameters()
-    body_runtime_param.detection_confidence_threshold = 40
-    bodies = sl.Bodies()
-    # Get ZED camera information
-    camera_info = zed.get_camera_information()
-    fps = camera_info.camera_configuration.fps
-    timestampList = []
-    keypoints = []
-    localorientations = []
-    localpositions = []
-    while zed.grab() == sl.ERROR_CODE.SUCCESS:
-        # Retrieve objects
-        zed.retrieve_bodies(bodies, body_runtime_param)
-        if bodies.is_new:
-            timestamp = bodies.timestamp
-            obj_array = bodies.body_list
-            if obj_array == []:
-                continue
-            first_object = obj_array[0]
-            keypoint = first_object.keypoint
-            localorientation = first_object.local_orientation_per_joint
-            localposition = first_object.local_position_per_joint	
-            # Update List
-            keypoints.append(keypoint)
-            localorientations.append(localorientation)
-            localpositions.append(localposition)
-            timestampList.append(timestamp.get_milliseconds())
-    # Disable modules and close camera
-    zed.disable_object_detection()
-    zed.disable_positional_tracking()
-    zed.close()
-    # print(sl.BODY_BONES_POSE_34)
-    savemat(output_path,{'timestampList':timestampList,'keypoints':keypoints,'fps':fps})
-    # print(len(timestampList))
-    # print(len(keypoints))
-    # print(len(keypoints[1]))
-    # print(svo_path+ "  " +output_path)
-
 if __name__ == "__main__":
-    file_dir = "/home/rzy/Documents/ZED/Data_1231/svos"
-    output_dir = "./data_new" # 注意不要将'./data'写为'./data/'
+    file_dir = "/home/rzy/Documents/ZED/Data_1231/svos/"
+    output_dir = "./data_new_18/" # 注意不要将'./data'写为'./data/'
+    body_format = 'BODY_18'
+    cam = sl.Camera()
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
     os.makedirs(output_dir,exist_ok=True)
-    copyDirectory_createMat(file_dir,output_dir)
+    copyDirectory_createMat(cam,file_dir,output_dir,body_format)
     print('finished.')
 
         
